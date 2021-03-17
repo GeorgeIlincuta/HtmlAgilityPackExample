30f71598d906521f5edf7d5af2b5021a142b06bc
GeorgeIlincuta <ilincuta.cosmin@gmail.com>
2021-03-18 00:19:55 +0200

    Added Pagination to scraper

--- Experience/Program.cs
+++ Experience/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading;
 using HtmlAgilityPack;
 
 namespace Experience
@@ -9,39 +10,47 @@ namespace Experience
     {
         static void Main(string[] args)
         {
+            var home = new List<Apartament>();
             var url = "https://www.olx.ro/imobiliare/apartamente-garsoniere-de-vanzare/bucuresti/";
             var web = new HtmlWeb();
-            var doc = web.Load(url);            
-            HtmlNodeCollection parentId = doc.DocumentNode.SelectNodes("//*[@id='offers_table']//*[@class='wrap']");
-            
-            var home = new List<Apartament>();
 
-            foreach (var item in parentId)
+            HtmlNode nextButton = null;
+
+            do
             {
-                var title         = item.SelectSingleNode(".//*[@class='lheight22 margintop5']").InnerText.Trim();
-                var price         = item.SelectSingleNode(".//*[@class='price']").InnerText.Trim();
-                var priceFormat   = Regex.Replace(price, @"[^\u0000-\u007F]", string.Empty);
-                var location      = item.SelectSingleNode(".//*[@class='lheight16']/small[1]/span/text()").InnerText.Trim();
-                var time          = item.SelectSingleNode(".//*[@class='lheight16']/small[2]/span/text()").InnerText.Trim();
+                var doc = web.Load(url);
+                HtmlNodeCollection parentId = doc.DocumentNode.SelectNodes("//*[@id='offers_table']//*[@class='wrap']");
+                foreach (var item in parentId)
+                {
+                    //Thread.Sleep(500);
+                    var title = item.SelectSingleNode(".//*[@class='lheight22 margintop5']").InnerText.Trim();
+                    var price = item.SelectSingleNode(".//*[@class='price']").InnerText.Trim();
+                    var priceFormat = Regex.Replace(price, @"[^\u0000-\u007F]", string.Empty);
+                    var location = item.SelectSingleNode(".//*[@class='lheight16']/small[1]/span/text()").InnerText.Trim();
+                    var time = item.SelectSingleNode(".//*[@class='lheight16']/small[2]/span/text()").InnerText.Trim();
 
-                if (time.Contains("Azi"))
-                {
-                    time = DateTime.Today.ToShortDateString();
-                }
-                else if (time.Contains("Ieri"))
-                {
-                    time = DateTime.Today.AddDays(-1).ToShortDateString();
-                }
+                    if (time.Contains("Azi"))
+                    {
+                        time = DateTime.Today.ToShortDateString();
+                    }
+                    else if (time.Contains("Ieri"))
+                    {
+                        time = DateTime.Today.AddDays(-1).ToShortDateString();
+                    }
 
-                home.Add(new Apartament()
-                {
-                    Title    = title,
-                    Price    = priceFormat,
-                    Location = location,
-                    Time     = time
-                });
-              
-            }
+                    home.Add(new Apartament()
+                    {
+                        Title = title,
+                        Price = priceFormat,
+                        Location = location,
+                        Time = time
+                    });
+
+                    nextButton = doc.DocumentNode.SelectSingleNode("//*[@class='fbold next abs large']/a");
+                    url = nextButton?.GetAttributeValue<string>("href", null);
+                }
+            } while (nextButton != null);
+
             foreach (var item in home)
             {
                 Console.WriteLine(item.Title);
